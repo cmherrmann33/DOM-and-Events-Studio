cbcba69d2acec825fc3514bffceb2cecb7b5ba75
Caitlin Herrmann <cmherrmann33@gmail.com>
2020-07-08 18:40:39 -0500

    Added Pt 2 requirements, functions not set up yet

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init() {
+    let takeoff = document.getElementById("takeoff");
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+}
+
+window.onload = init;
