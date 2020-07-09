049c2132ec162632c16acfeccb9ff1aaf3142d4e
Caitlin Herrmann <cmherrmann33@gmail.com>
2020-07-08 19:22:27 -0500

    2/3 working. Cannot get color change to work

--- scripts.js
+++ scripts.js
@@ -4,8 +4,19 @@
 function init() {
     let takeoff = document.getElementById("takeoff");
     let flightStatus = document.getElementById("flightStatus");
-    let shuttleBackground = document.getElementById("shuttleBackground");
+    let shuttleBackground = document.querySelector("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+
+    takeoff.addEventListener("click", function() {
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff")
+        if (response) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            spaceShuttleHeight.innerHTML = "10,000";
+            shuttleBackground.background = "blue";
+        } 
+    });
+
 }
 
 window.onload = init;
